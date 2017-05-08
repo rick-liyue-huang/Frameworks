adb76f294f25cf950d288c01bee22b3873a0e3a8
rickliyuehuang <rick.liyue.huang@gmail.com>
2017-05-08 23:33:04 +1000

    modify the first part

--- Framework-By-Simulating-jQuery/js/common.js
+++ Framework-By-Simulating-jQuery/js/common.js
@@ -1,104 +1,103 @@
-/**
- * Created by rickhuang on 19/12/16.
- */
 
-// 对基本方法的封装
+    /*
+        Created by rickhuang on 19/11/16
+        modified by RickHuang on 08/05/17
+    */
 
-var myPush = function (target, els) {
+    // encap the basic methods
 
-    var j = target.length,
-        i = 0;
-    while ((target[j++] = els[i++])) {}
-    target.length = j - 1; // 伪数组 的长度是不会随着加入的元素而增加。
-};
+    let support = {};
+    support.getElementsByClassName = !!document.getElementsByClassName;
+    support.getElementsByClassName = (function() {
+        let isExist = document.getElementsByClassName;
+        if (isExist && typeof document.getElementsByClassName) {
 
+            let div = document.createElement('div');
+            let divWithClass = document.createElement('div');
+            divWithClass.className = 'c';
+            div.appendChild(divWithClass);
+            return div.getElementsByClassName === divWithClass;
+        }
+        return false;
+    })();
 
-var getTag = function (tagName, results) {
-    results = results || [];
-    try {
-        results.push.apply(results, document.getElementsByTagName(tagName));
-    } catch (e) {
-        myPush(results, document.getElementsByTagName(tagName));
-    }
-    return results;
-};
-
-var getId = function (idName, results) {
-    results = results || [];
-    try {
-        results.push(document.getElementById(idName));
-    } catch (e) {
-        myPush(document.getElementById(idName));
-    }
-    return results;
-};
-
-var getClass = function (className, results) {
-    results = results || [];
-
-    var tempArr;
-    if (document.getElementsByClassName) {
-        results.push.apply(results, document.getElementsByClassName(className));
-    } else {
-
-        each(getTag("*"), function (i, v) {
-            if ((' ' + v.className + ' ').indexOf(' ' + className + ' ') != -1) {
-                results.push(v);
+    let each = function(arr, fn) {
+        for (let i = 0; i < arr.length; i++) {
+            if (fn.call(arr[i], i, arr[i]) === false) {
+                break;
             }
-        });
-    }
-    return results;
-};
-
-
-// 对循环的封装
-
-var each = function (arr, fn) {
-    for (var i = 0; i < arr.length; i++) {
-        if (fn.call(arr[i], i, arr[i]) === false) {
-            break;
         }
+    };
+
+    let rickPush = function(target, els) {
+
+        let j = target.length,
+            i = 0;
+
+        while((target[j++] = els[i++])) {}
+        target.length = j - 1; // the pseudo-array length can not change
     }
-};
 
-// get 方法封装 -- 通用的方法
-var get = function (selector, results) {
-    results = results || [];
+    let getTag = function(tagName, results) {
+        results = results || [];
 
-    var rquickrExp = /^(?:#([\w-]+)|\.([\w-]+)|([\w]+)|\*)$/,
-        m = rquickrExp.exec(selector);
-
-    if (m) {
-        if (m[1]) {
-            results = getId(m[1], results);
-        } else if (m[2]) {
-            results = getClass(m[2], results);
-        } else if (m[3]) {
-            results = getTag(m[3], results);
-        } else if (m[4]) {
-            results = getTag(m[4], results);
+        try {
+            results.push.apply(results, document.getElementsByTagName(tagName));
+        } catch (e) {
+            rickPush(results, document.getElementsByTagName(tagName));
         }
+        return results;
+    };
+
+    let getId = function(idName, results) {
+        results = resutls || [];
+
+        try {
+            results.push.apply(results, document.getElementById(idName));
+        } catch(e) {
+            rickPush(results, document.getElementById(idName));
+        }
+        return results;
     }
-    return results;
-};
 
 
+    let getClass = function(className, results) {
 
+        results = results || [];
 
+        let tempArr = [];
+        if (support.getElementsByClassName) {
+            results.push.apply(results, document.getElementsByClassName(className));
+        } else {
 
+            each(getTag('*'), function(i, v) {
+                if ((' ' + v.className + ' ').indexOf(' ' + className + ' ') != -1) {
+                    results.push(v);
+                }
+            });
+        }
+        return results;
+    };
 
+    let get = function(selector, results) {
+        resutls = results || [];
 
+        let rquickExp = /^(?:#([\w-]+)|\.([\w-]+)|([\w]+)|\*)$/;
+        let m = rquickExp.exec(selector);
 
-
-
-
-
-
-
-
-
-
-
+        if (m) {
+            if (m[1]) {
+                results = getId(m[1], results);
+            } else if (m[2]) {
+                results = getClass(m[2], results);
+            } else if (m[3]) {
+                results = getTag(m[3], results);
+            } else if (m[4]) {
+                results = getTag(m[4], results);
+            }
+        }
+        return results;
+    };
 
 
 
